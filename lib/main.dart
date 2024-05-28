b621e78b31f6f1ee993130c19fbb7c1f787de9d7
MGRyan13 <132696403+MGRyan13@users.noreply.github.com>
2024-05-29 00:09:34 +0100

    added logo

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:project_mini/Admin/dashboard.dart';
 import 'package:project_mini/Admin/navigationBarAdmin.dart';
 import 'package:project_mini/auth/fetch_role.dart';
 import 'package:project_mini/auth/login_page.dart';
